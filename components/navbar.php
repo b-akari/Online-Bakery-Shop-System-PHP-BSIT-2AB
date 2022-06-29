80b2231dad29b96d5c207c2890a94ef63af3fb0f
Wyn Chrsitian <wynchristian.rebanal@tup.edu.ph>
2022-06-29 18:31:50 +0800

    update

---
+++ components/navbar.php
@@ -0,0 +1,17 @@
+<div class="container">
+  <div class="navbar">
+    <div class="logo">
+      <a href="index.php"><img src="images/logo.png" width="200px"></a>
+    </div>
+    <nav>
+      <ul id="MenuItems">
+        <li><a href="menu.php">Menu</a></li>
+        <li><a href="#">Best Seller</a></li>
+        <li><a href="#">Customize</a></li>
+      </ul>
+    </nav>
+    <a href="#"><img src="images/cart.png" width="25px" height="25px"></a>
+    <a href=""><i class="fa-solid fa-user"></i></a>
+    <img src="images/menu.png" class="menu_icon" onclick="menutoggle()">
+  </div>
+</div>
